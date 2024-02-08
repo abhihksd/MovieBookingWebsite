0f5aba7e9654d9c4ded45e007126cccecdb0d27b
Yache Shubham <77227410+Shubhamyache@users.noreply.github.com>
2024-02-08 12:20:53 +0530

    movie details page
    movie detail page which will pop up after clicking on movie

---
+++ movie/src/components/MovieDetailsAfterClick.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../css/MovieDetails.css'; 
+
+const MovieDetails = ({ movie }) => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-4">
+          <img src={movie.posterUrl} alt={movie.title} className="img-fluid movie-poster" />
+        </div>
+        <div className="col-md-8">
+          <div className="movie-info">
+            <h2 className="movie-title">{movie.title}</h2>
+            <p className="movie-description">{movie.description}</p>
+            <p className="movie-cast"><strong>Cast:</strong> {movie.cast}</p>
+            <button className="btn btn-primary">Book Now</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MovieDetails;
