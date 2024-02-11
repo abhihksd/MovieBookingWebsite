f4ae56477750e12f3bd0c806a01c46a799146575
Yache Shubham <77227410+Shubhamyache@users.noreply.github.com>
2024-02-11 10:31:29 +0530

    new
    changes

--- movie-ticket/src/App.js
+++ movie-ticket/src/App.js
@@ -9,33 +9,32 @@ import CustomerHome from './Components/CustomerHome';
 function App() {
   return (
     <div className="App">
-     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <Link className="navbar-brand toplogo" to="/"><img src={logo}  /></Link>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
-  <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div className="navbar-nav">
-      <Link className="nav-item nav-link active" to="/" >Home <span className="sr-only">(current)</span></Link>
-      <Link className="nav-item nav-link"  to="/login">Login</Link>
-      <Link className="nav-item nav-link"  to="/register">Register</Link>
-    </div>
-  </div>
-</nav>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link className="navbar-brand toplogo" to="/"><img src={logo}  /></Link>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav">
+          <Link className="nav-item nav-link active" to="/" >Home <span className="sr-only">(current)</span></Link>
+          <Link className="nav-item nav-link"  to="/login">Login</Link>
+          <Link className="nav-item nav-link"  to="/register">Register</Link>
+        </div>
+      </div>
+    </nav>     
 
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/register' element={<RegForm/>}  />
+      <Route path="/customer_home" element={<CustomerHome/>} />
+    </Routes>
 
-     
-
-<Routes>
-  <Route path='/' element={<Home/>}/>
-  <Route path='/login' element={<Login/>}/>
-  <Route path='/register' element={<RegForm/>}  />
-  <Route path="/customer_home" element={<CustomerHome/>} />
-</Routes>
-
-    </div>
+</div>
 
   );
+ 
 }
+
 
 export default App;
