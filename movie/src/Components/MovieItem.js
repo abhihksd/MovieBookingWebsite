14174ed0eda4fdc050ff5deb9fd2708357a69495
Abhishek Shujalpurkar <46176211+abhihksd@users.noreply.github.com>
2024-02-17 22:48:55 +0530

    Add files via upload

--- movie/src/Components/MovieItem.js
+++ movie/src/Components/MovieItem.js
@@ -1,23 +1,40 @@
-import React from "react";
-
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-import m1 from "../img/M1.jpg";
+import M1 from"../img/M1.jpg"
 
-const NewsItem = (props) => {
+import '../CSS/card.css';
+import MovieDetails from "./MovieDetailsAfterClick";
+import { useNavigate } from "react-router";
+import { Link } from "react-router-dom";
+const MovieItem = ({ movie_id, title, description, release_date, genre, duration, language, director, imageurl }) => {
+  const nav=useNavigate();
+  const [mobject,setMobject]=useState({ movie_id, title, description, release_date, genre, duration, language, director, imageurl })
+ const [showDetails, setShowDetails] = useState(false);
+ 
   return (
-    <Card style={{ width: "14rem" }}>
-      <Card.Img variant="top" src={m1} />
-      <Card.Body>
-        <Card.Title>Fighter</Card.Title>
-        <Card.Text>
-          Shamsher Pathania fulfills his lifelong dream and becomes a member of
-          the Indian air force.
-        </Card.Text>
-        <Button variant="primary">Book now</Button>
-      </Card.Body>
-    </Card>
+    <div>
+      
+      <Card style={{ width: "14rem" }}>
+        <Card.Img variant="top" />
+        <Card.Img variant="top" src={M1} alt={`Image for ${title}`} />
+        <Card.Body>
+          <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{ left: '85%', zIndex: 1 }}>{genre}</span>
+          <Card.Img variant="top" />
+          <Card.Title className="text-center" >{title}</Card.Title>
+          <Card.Text>{movie_id}</Card.Text>
+          <Card.Text>{director}</Card.Text>
+          <Card.Text>{release_date}</Card.Text>
+          {showDetails && <MovieDetails movie={mobject} />}
+          <Link to={{ pathname:'/selectMovie',state:{mobject}}}>
+          View Movie
+           </Link>
+          <Button variant="primary"  onClick={() => setShowDetails(!showDetails)} >View</Button>
+        </Card.Body>
+      </Card>
+      
+    </div>
   );
 };
 
-export default NewsItem;
+export default MovieItem;
