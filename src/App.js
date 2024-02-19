16d28a1d9706cda221f8f16f3174cb5ba99dafed
prathamesh3559 <pratha.hanmante@gmail.com>
2024-02-19 12:33:20 +0530

    Updated Delete Movie feature

---
+++ src/App.js
@@ -0,0 +1,90 @@
+import logo from "./logo.svg";
+import bike from "./img/logo.jpeg";
+import "./App.css";
+
+import Register from "./Components/RegistrationForm";
+import { Link, Route, Routes } from "react-router-dom";
+import LoginForm from "./Components/LoginForm";
+import Home from "./Components/Home";
+import { useSelector } from "react-redux";
+import LogoutComp from "./LogoutComp";
+import UpdatePass from "./Components/UpdatePass";
+import OrderHis from "./Components/OrderHistory";
+import Welcome from "./Components/Welcome";
+
+import SystemAdmin from "./Components/SystemAdmin";
+import TheatreAdmin from "./Components/TheatreAdmin";
+import User from "./Components/User";
+import AddSlot from "./Components/Theatre_AddSlot";
+import EditSlot from "./Components/Theatre_EditSlot";
+import ViewSchedule from "./Components/Theatre_ViewSchedule";
+import RemoveMovie from "./Components/Theatre_RemoveMovie";
+import AddMovie from "./Components/Theatre_AddMovie";
+import EditMovie from "./Components/Theatre_EditMovie";
+import AddTheater from "./Components/System_AddTheatre";
+import EditTheatre from "./Components/System_EditTheatre";
+import RemoveTheatre from "./Components/System_RemoveTheatre";
+import BookMovie from "./Components/User_BookMovie";
+
+import FetchData from './Components/FetchData';
+import MovieItem from './Components/MovieItem';
+function App() {
+  //initialState of logged
+  const mystate = useSelector((state) => state.logged);
+  return (
+    <div>
+      <header
+        // className="App-header"
+        style={{ minHeight: mystate.loggedIn ? "0vh" : "auto" }}
+      >
+        <div style={{ display: mystate.loggedIn ? "none" : "block" }}>
+          <ul className="navbar navbar-expand-sm bg-light mb-3">
+            <div className="topnav">
+              <li className="nav-item">
+                <img className="App-logo" src={bike} alt="Logo" />
+              </li>
+              <div className="topnav-right">
+                <li className="nav-item search-field">
+                  <input className="search" type="text" placeholder="Search" />
+                </li>
+
+                <li className="nav-item">
+                  <Link to="/register">Register</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/login">Login</Link>
+                </li>
+              </div>
+            </div>
+          </ul>
+        </div>
+      </header>
+      <div>
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/systemAdmin" element={<SystemAdmin />} />
+          <Route path="/theatreAdmin" element={<TheatreAdmin />} />
+          <Route path="/user" element={<User />} />
+          <Route path="/logout" element={<LogoutComp />} />
+          <Route path="/addSlot" element={<AddSlot />} />
+          <Route path="/editSlot" element={<EditSlot />} />
+          <Route path="/addMovie" element={<AddMovie />} />
+          <Route path="/viewSchedule" element={<ViewSchedule />} />
+          <Route path="/editMoive" element={<EditMovie />} />
+          <Route path="/removeMovie" element={<RemoveMovie />} />
+          <Route path="/AddTheater" element={<AddTheater />} />
+          <Route path="/removeTheatre" element={<RemoveTheatre />} />
+          <Route path="/editTheatre" element={<EditTheatre />} />
+          <Route path="/bookMovie" element={<BookMovie />} />
+
+          
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/FetchData.js
@@ -0,0 +1,88 @@
+// import React, { useState, useEffect } from 'react';
+// import MovieItem from './MovieItem';
+// //import '../css/card.css';                                    //to review
+
+// function FetchData() {
+//     const [movies, setMovies] = useState([]);
+
+//     useEffect(() => {
+//         fetchMovies();
+//     }, []);
+
+//     const fetchMovies = async () => {
+//         try {
+//             const response = await fetch('http://localhost:8080/movies/getAllMovies');
+//             const data = await response.json();
+//             setMovies(data);
+//         } catch (error) {
+//             console.error('Error fetching movies:', error);
+//         }
+//     };
+
+//     return (
+//         <div>
+//             <div className="card-container">
+//             {movies.map((movie) => (
+//                 <MovieItem
+//                     key={movie.movie_id}
+//                     title={movie.title}
+//                     release_date={movie.release_date}
+//                     genre={movie.genre}
+//                     duration={movie.duration}
+//                     language={movie.language}
+//                     director={movie.director}
+//                     description={movie.description}
+                   
+//                 />
+//             ))}
+//             </div>
+//         </div>
+//     );
+// }
+
+// export default FetchData;
+
+import React, { useState, useEffect } from 'react';
+import MovieItem from './MovieItem';
+import '../CSS/card.css';
+
+function FetchData() {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        fetchMovies();
+    }, []);
+
+    const fetchMovies = async () => {
+        try {
+            const response = await fetch('http://localhost:8080/movies/getAllMovies');
+            const data = await response.json();
+            setMovies(data);
+        } catch (error) {
+            console.error('Error fetching movies:', error);
+        }
+    };
+
+    return (
+        <div>
+            <div className="card-container">
+            {movies.map((movie) => (
+                <MovieItem
+                    key={movie.movie_id}
+                    movie_id={movie.movie_id}
+                    title={movie.title}
+                    release_date={movie.release_date}
+                    genre={movie.genre}
+                    duration={movie.duration}
+                    language={movie.language}
+                    director={movie.director}
+                    description={movie.description}
+                   
+                />
+            ))}
+            </div>
+        </div>
+    );
+}
+
+export default FetchData;
