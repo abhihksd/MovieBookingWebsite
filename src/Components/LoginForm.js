974aac714ef37fc45227df80f2614488b25d93b8
prathamesh3559 <pratha.hanmante@gmail.com>
2024-02-21 07:58:42 +0530

    .net done

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -901,6 +901,8 @@ export default function LoginForm() {
             />
           </div>
         </div>
+
+          
         <div className="row">
           <div className="col-sm-2"></div>
           <div className="col-sm-10">
